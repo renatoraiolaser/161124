5c93f9c9dfdb3eb00b03201c6026976c6f0edcdd
renatoraiolaser <marquesrenato97@gmail.com>
2024-11-24 23:06:07 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const cookiePopup = document.getElementById('cookie-popup');
     const acceptButton = document.getElementById('accept-cookies');
 
-    // Verificar se o usuário já aceitou os cookies
-    if (localStorage.getItem('cookiesAccepted') === 'true') {
-        cookiePopup.classList.add('hidden');
-    }
-
     // Ação ao clicar no botão de aceitação
     acceptButton.addEventListener('click', function () {
-        localStorage.setItem('cookiesAccepted', 'true'); // Salva a aceitação no localStorage
         cookiePopup.classList.add('hidden'); // Esconde o popup
         setTimeout(() => {
-            cookiePopup.remove(); // Remove completamente o elemento do DOM após a animação
-        }, 300); // Tempo da transição definida no CSS (0.3s)
+            cookiePopup.remove(); // Remove o elemento do DOM após a animação
+        }, 300); // Tempo da transição (300ms)
     });
 });
 
